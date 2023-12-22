658aef677113db01138613e1d1d3d4d55f44d47c
hachimB <boubacarhachim@gmail.com>
2023-12-22 23:45:26 +0100

    OK

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
 # This script install flask from pip3 using puppet
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip',
+  provider => 'pip3',
+}
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }

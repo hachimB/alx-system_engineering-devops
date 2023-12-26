886b63748ad0ac4612de36b64249d70546a02399
hachimB <boubacarhachim@gmail.com>
2023-12-26 23:31:40 +0100

    ok

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,11 @@
 # Client configuration file (w/ Puppet)
-file { "${HOME}/.ssh/config":
+file { "${facts['home']}/.ssh":
+  ensure => 'directory',
+}
+
+file { "${facts['home']}/.ssh/config":
   ensure  => 'file',
   content => "HostName 35.175.130.80
 User ubuntu
-IdentityFile ${HOME}/.ssh/school",
+IdentityFile ${facts['home']}/.ssh/school",
 }

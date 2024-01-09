44d7a4eff8964b0ddf5e9d9cc76bd18baf75e00c
hachimB <boubacarhachim@gmail.com>
2024-01-09 01:49:26 +0100

    OK

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,14 +1,14 @@
 class { 'nginx': }
 
 nginx::resource::server { 'default':
-  listen_port             => 80,
-  index_files             => ['index.html', 'index.htm', 'index.php'],
-  use_default_location    => false,
-  www_root                => '/var/www/html',
-  locations               => {
-    'default'             => {
+  listen_port          => 80,
+  index_files          => ['index.html', 'index.htm', 'index.php'],
+  use_default_location => false,
+  www_root             => '/var/www/html',
+  locations            => {
+    'default' => {
       location            => '/',
-      location_custom_cfg => { 'add_header' => "X-Served-By ${::hostname}" },
+      location_custom_cfg => { 'add_header' => "X-Served-By ${hostname}" },
     },
   },
 }

a29bcfbb4b28905835b615cc5ff375f3023a4a19
hachimB <boubacarhachim@gmail.com>
2024-02-13 18:50:45 +0100

    ok

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,11 +4,10 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    headers = {"User-Agent": "MyBot/1.0"}
+    headers = {"User-Agent": "Mozilla/5.0"}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        if 'data' in data and 'subscribers' in data['data']:
-            return data['data']['subscribers']
+        return data['data']['subscribers']
     return 0

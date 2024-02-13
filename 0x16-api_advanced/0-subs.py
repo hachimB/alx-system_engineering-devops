4aa053cb66a8658a4dab6e060575aca73aa74bf2
hachimB <boubacarhachim@gmail.com>
2024-02-13 19:05:29 +0100

    ok

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """number of subcribers"""
+    """number of subscribers"""
     headers = {"User-Agent": "Mozilla/5.0"}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     response = requests.get(url, headers=headers, allow_redirects=False)

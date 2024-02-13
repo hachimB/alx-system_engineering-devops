e2154981ded802a3be026970788d2f46ebb72820
hachimB <boubacarhachim@gmail.com>
2024-02-13 18:38:34 +0100

    ok

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,9 +4,10 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    headers = {"User-Agent": "api"}
+    headers = {"User-Agent": "MyBot/1.0"}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        return response.json()['data']['subscribers']
+        data = response.json()
+        return data['data']['subscribers']
     return 0
